927b6b465cb08dc49f53bc4bfdb75255290a953a
Ruiyu666 <152041374+Ruiyu666@users.noreply.github.com>
2023-11-29 00:12:38 -0800

    Add files via upload

--- assignment.js
+++ assignment.js
@@ -1,48 +1,55 @@
-const country = 'China';
-const continent = 'Asia';
-var population = 14000; 
+let country = 'Canada';
+let population = '40';
+let language = 'English'
+let island = false;
 
-console.log(country);
-console.log(continent);
-console.log(population);
+if (language == 'English'&& population < 50 && !island)
+{console.log(`You should live in ${country} :)`); 
+} else { 
+  console.log (`${country} does not meet your criteria:(`); }
 
-const island = false; 
-const language = 'Chinese';
 
-console.log(typeof island);
-console.log(typeof population);
-console.log(typeof country);
-console.log(typeof language);
 
-console.log(population / 2);
-population++;
-console.log(population);
-console.log(population > 6);
-console.log(population < 33);
 
-var description = 'China is in Asia, and its 14 Billions people speak Chinese';
-console.log(description);
+  //
+let languages = 'english';
 
-const description1 = `${country} is in ${continent}, and its ${population} Millions people speak ${language}`;
-console.log(description1);
+  switch (languages) 
+  {
+    case 'Chinese': 
+    case 'Mandarin':
+    console.log(`Most nember of native speakers!`);
+    break;
 
-if (population > 33);
-console.log(`${country}'s population is above average` );
-if (population <33);
-console.log(`${country}'s population is ${33-population} million below average`);
+    case 'spanish':
+    console.log(`2nd place in number of native speakers`);
+    break;
 
-console.log('9'-'5');
-console.log('19'-'13'+'17');
-console.log('19'-'13'+ 17);
-console.log('123'<57);
-console.log(5+6+'4'+9-4-2);
+    case 'english':
+    console.log(`3rd!`);
+    break;
 
-var numNeighbours = '1'
-if (numNeighbours==1)
-console.log(`Only 1 border!`);
+    case 'hindi':
+    console.log(`number 4`);
+    break;
 
-if (numNeighbours > 1)
-console.log(`More than 1 border`);
+    case 'arabic':
+    console.log (`5th most sopken language!`);
+    break;
 
-if (numNeighbours==0)
-console.log(`No borders`);
+
+  default:
+    console.log(`Great language too :D`)
+
+  }
+
+
+  //
+  let country1 = 'Canada';
+  let population1 = '130';
+  
+  if (population1 > 33) {
+    console.log(`${country1}'s population is above average`);
+  } else {
+    console.log(`${country1}'s population is below average`);
+  }
