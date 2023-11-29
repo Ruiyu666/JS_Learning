2924f727f9987035f57fda7b467dea16e786c71e
Ruiyu666 <152041374+Ruiyu666@users.noreply.github.com>
2023-11-28 23:04:50 -0800

    JS Lesson 1 practice

---
+++ assignment.js
@@ -0,0 +1,48 @@
+const country = 'China';
+const continent = 'Asia';
+var population = 14000; 
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+const island = false; 
+const language = 'Chinese';
+
+console.log(typeof island);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+console.log(population / 2);
+population++;
+console.log(population);
+console.log(population > 6);
+console.log(population < 33);
+
+var description = 'China is in Asia, and its 14 Billions people speak Chinese';
+console.log(description);
+
+const description1 = `${country} is in ${continent}, and its ${population} Millions people speak ${language}`;
+console.log(description1);
+
+if (population > 33);
+console.log(`${country}'s population is above average` );
+if (population <33);
+console.log(`${country}'s population is ${33-population} million below average`);
+
+console.log('9'-'5');
+console.log('19'-'13'+'17');
+console.log('19'-'13'+ 17);
+console.log('123'<57);
+console.log(5+6+'4'+9-4-2);
+
+var numNeighbours = '1'
+if (numNeighbours==1)
+console.log(`Only 1 border!`);
+
+if (numNeighbours > 1)
+console.log(`More than 1 border`);
+
+if (numNeighbours==0)
+console.log(`No borders`);
