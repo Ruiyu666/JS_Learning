5851d23013ade777c73a628de33757aeba902d3a
Ruiyu666 <152041374+Ruiyu666@users.noreply.github.com>
2023-12-02 21:30:40 -0800

    Challenge1

---
+++ challenge1.js
@@ -0,0 +1,20 @@
+/* Write your code below. Good luck! 🙂 */
+
+/*let massMark = 78;
+let heightMark = 1.69;
+let massJohn = 92;
+let heightJohn = 1.95;*/
+
+let massMark = 95;
+let heightMark = 1.88;
+let massJohn = 85;
+let heightJohn = 1.76;
+
+let BMIMark= massMark / (heightMark * heightMark);
+console.log (BMIMark);
+
+let BMIJohn = massJohn /(heightJohn*heightJohn);
+console.log (BMIJohn);
+
+const markHigherBMI = BMIMark > BMIJohn;
+console.log (markHigherBMI);
